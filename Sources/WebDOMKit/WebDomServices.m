6b4f6ffbad0e3780f7f70b99623b7f579c452993
danny1113 <danny901113@gmail.com>
2022-02-28 14:13:26 +0800

    Remove `initWithFrame:NSZeroRect`.

--- Sources/WebDOMKit/WebDomServices.m
+++ Sources/WebDOMKit/WebDomServices.m
@@ -64,7 +64,7 @@
 
 
 - (void)setupWebView {
-    webView = [[WKWebView alloc] initWithFrame:NSZeroRect];
+    webView = [[WKWebView alloc] init];
     webView.navigationDelegate = self;
     webView.customUserAgent = @"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15";
 }
